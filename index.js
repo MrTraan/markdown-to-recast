ec10a897342a58c42c841683df9bf1b9ddd9c9e6
Nathan Grasset <nathan.grasset@recast.ai>
2018-01-24 13:37:04 +0100

    support more formats: links, header, italic and bold

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ function concatArrays (iter, item) {
   return iter.concat(item);
 }
 
+function extractText (item) {
+  if (typeof item === 'string') {
+    if (/^\s+$/.test(item)) {
+      return null;
+    }
+    return formats.text(item.trim());
+  }
+  return parseNode(item);
+}
+
 function parseNode (node) {
   var type = node[0];
 
@@ -13,28 +23,35 @@ function parseNode (node) {
     case 'img':
       return [formats.image(node[1].href)];
     case 'para':
-      return node.slice(1).map(function (elem) {
-        if (typeof elem === 'string') {
-          if (/^\s+$/.test(elem)) {
-            return null;
-          }
-          return formats.text(elem.trim());
-        }
-        return parseNode(elem);
-      }).reduce(concatArrays, []);
+    case 'em':
+    case 'strong':
+    case 'code_block':
+      return node.slice(1)
+        .map(extractText)
+        .reduce(concatArrays, []);
+    case 'header':
+      return node.slice(2)
+        .map(extractText)
+        .reduce(concatArrays, []);
+    case 'link':
+      var details = node[1];
+      var text = node[2];
+      if (details.href !== text) {
+        return formats.text(text + ' (' + details.href + ')');
+      }
+      return formats.text(details.href);
     default:
       console.error('unsupported format :', node);
       return null;
   }
 }
 
-module.exports = function (md) {
+function parseText (md) {
   if (typeof md !== 'string') {
     throw new TypeError('first argument must be a string');
   }
 
   var tree = markdown.markdown.parse(md);
-
   assert(tree[0] === 'markdown');
 
   return tree
@@ -43,3 +60,5 @@ module.exports = function (md) {
     .reduce(concatArrays, [])
     .filter(function (elem) { return !!elem; });
 }
+
+module.exports = parseText;
